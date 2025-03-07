d36a4c546f49820625005bfc2442e4245ebf63d5
Bhuvnesh <bhuvnesh.karakoti@gmail.com>
2025-03-07 12:18:04 +0530

    setting up review models and error handlers

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
+//Express Dependencies
 const express = require('express');
+const app = express();
 const path = require('path');
-const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+
+app.engine('ejs', ejsMate);
+app.set('view engine','ejs');;
+app.set('views',path.join(__dirname,'views'));
+
+const mongoose = require('mongoose');
 const {campgroundSchema} = require('./schemas.js')
 const Campground = require('./models/campgrounds.js');
-const methodOverride = require('method-override');
-const campgrounds = require('./models/campgrounds.js');
+
 const ExpressError = require('./utils/ExpressError.js');
 const catchAsync = require('./utils/catchAsync.js');
-const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const Review = require('./models/review.js')
+
+// Mongoose Connection
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');                  
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"connection error:"));
 db.once("open",()=>{
     console.log("Database connected");
 });
 
-app.engine('ejs', ejsMate);
-app.set('view engine','ejs');;
-app.set('views',path.join(__dirname,'views'));
- 
-app.use(express.urlencoded({extended:true}));                                 //middleware added
+const methodOverride = require('method-override');
+app.use(express.urlencoded({extended:true}));                      //middleware added
 app.use(methodOverride('_method'));
 
-const validateCampground = (req,res,next)=>{                                   //validation middleware
-
+const validateCampground = (req,res,next)=>{                       //validation middleware
     const {error} = campgroundSchema.validate(req.body);
     if(error){
         const msg = error.details.map(el=>el.message).join(',')
@@ -34,7 +38,6 @@ const validateCampground = (req,res,next)=>{                                   /
         next();
     }
 }
-
 
 app.get('/',(req,res)=>{
     res.render('home')
@@ -45,16 +48,18 @@ app.get('/campgrounds', catchAsync(async (req,res)=>{
     res.render('campgrounds/index.ejs',{campgrounds});
 }));
 
-app.get('/campgrounds/new', async (req,res)=>{                               // adding new campgrounds to the database
+app.get('/campgrounds/new', async (req,res)=>{                     // adding new campgrounds to the database
     res.render('campgrounds/new');
 });
+
 app.post('/campgrounds', validateCampground,catchAsync(async(req,res,next)=>{
-    const campground = new Campground(req.body.campgrounds);
+    const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
-app.get('/campgrounds/:id', validateCampground, catchAsync(async (req,res)=>{                                //opening individual campground element by id
+
+app.get('/campgrounds/:id', catchAsync(async (req,res)=>{                                //opening individual campground element by id
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show.ejs',{campground});
 })) ;
@@ -72,19 +77,29 @@ app.put('/campgrounds/:id', async(req,res) =>{
 
 app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     const{id}=req.params;
-    await campgrounds.findByIdAndDelete(id);
+    await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
+}));
+
+app.post('/campgrounds/:id/reviews',catchAsync(async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+
 }));
 
 app.all('*', (req,res,next)=>{
     next(new ExpressError('Page Not Found', 404))
-})
+});
 
 app.use((err,req,res,next)=>{  
     if(!err.message) err.message='Oh no !!! Something went wrong!'
     const {statusCode = 500, message = 'Something went Wrong'} = err;
     res.status(statusCode).render('error',{err})                               // error handling
-})
+});
 
 app.listen(3000,()=>{
     console.log('Serving port 3000')
