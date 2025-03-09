6ba9cbeab91f6466ea44361ecdec5e8c09a19ae3
Bhuvnesh <bhuvnesh.karakoti@gmail.com>
2025-03-09 12:38:51 +0530

    review model setup

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine','ejs');;
 app.set('views',path.join(__dirname,'views'));
 
 const mongoose = require('mongoose');
-const {campgroundSchema} = require('./schemas.js')
+const {campgroundSchema,reviewSchema} = require('./schemas.js')
 const Campground = require('./models/campgrounds.js');
 
 const ExpressError = require('./utils/ExpressError.js');
@@ -39,6 +39,16 @@ const validateCampground = (req,res,next)=>{                       //validation 
     }
 }
 
+const validateReview = (req,res,next)=>{
+    const{error} = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }else{
+        next();
+    }
+}
+
 app.get('/',(req,res)=>{
     res.render('home')
 });
@@ -60,7 +70,8 @@ app.post('/campgrounds', validateCampground,catchAsync(async(req,res,next)=>{
 
 
 app.get('/campgrounds/:id', catchAsync(async (req,res)=>{                                //opening individual campground element by id
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground);
     res.render('campgrounds/show.ejs',{campground});
 })) ;
 
@@ -81,7 +92,7 @@ app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     res.redirect('/campgrounds')
 }));
 
-app.post('/campgrounds/:id/reviews',catchAsync(async(req,res)=>{
+app.post('/campgrounds/:id/reviews',validateReview,catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
@@ -91,6 +102,7 @@ app.post('/campgrounds/:id/reviews',catchAsync(async(req,res)=>{
 
 }));
 
+app.post('/campgroeunds/:id/reviews',validateReview)
 app.all('*', (req,res,next)=>{
     next(new ExpressError('Page Not Found', 404))
 });
